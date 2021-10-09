52a9c6e8e5df4116f211d8d573d29a221871be78
racoonbest <91798907+racoonbest@users.noreply.github.com>
2021-10-09 11:48:32 -0400

    Update TestRunner.java

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/features/"},
-        plugin = { "pretty", "html:target/cucumber-reports.html" }
+        plugin = { "pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json" }
 )
 
 public class TestRunner {
