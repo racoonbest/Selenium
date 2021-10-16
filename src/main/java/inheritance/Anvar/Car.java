ded7fb67ebc556c15ae7fc391dfd4a107c9f45a0
anvark1976 <92128415+anvark1976@users.noreply.github.com>
2021-10-16 12:38:27 -0400

    Anvars code

--- src/main/java/inheritance/Anvar/Car.java
+++ src/main/java/inheritance/Anvar/Car.java
@@ -1,4 +1,14 @@
 package inheritance.Anvar;
 
 public class Car {
+
+        Car running
+
+    public static void main(String[] args) {
+
+        Car Toyota = new Car();
+        Toyota.running();
+
+
+    }
 }
