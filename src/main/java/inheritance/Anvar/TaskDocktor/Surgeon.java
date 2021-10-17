9d7f60d4207724f1972103b05b30cec3c387666d
anvark1976 <92128415+anvark1976@users.noreply.github.com>
2021-10-17 14:14:33 -0400

    Anvar

---
+++ src/main/java/inheritance/Anvar/TaskDocktor/Surgeon.java
@@ -0,0 +1,9 @@
+package inheritance.Anvar.TaskDocktor;
+
+public class Surgeon extends Doctors {
+
+    public void surgery(){
+        System.out.println("Surgeon should be able to perform surgery");
+    }
+
+}
