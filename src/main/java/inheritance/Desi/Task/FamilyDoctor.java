d4736624a7584e7896c1a0116c9c40e7ee4128c3
desislavalyubomirova <92128196+desislavalyubomirova@users.noreply.github.com>
2021-10-17 15:07:30 -0400

    Desi's code

--- src/main/java/inheritance/Desi/Task/FamilyDoctor.java
+++ src/main/java/inheritance/Desi/Task/FamilyDoctor.java
@@ -1,6 +1,6 @@
 package inheritance.Desi.Task;
 
-public class FamilyDoctor {
+public class FamilyDoctor extends MainDoctor {
     public void giveAdvice() {
         System.out.println("I'm giving advice");
 
