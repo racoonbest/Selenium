8d6c39d7d7e1f7c75f08a04fe44d93efc28b58d7
zarshed1997 <92128200+zarshed1997@users.noreply.github.com>
2021-10-20 19:11:56 -0400

    test

--- src/main/java/inheritance/Desi/FoodHomework/Main7.java
+++ src/main/java/inheritance/Desi/FoodHomework/Main7.java
@@ -2,30 +2,26 @@ package inheritance.Desi.FoodHomework;
 
 public class Main7 {
     public static void main(String[] args) {
-        Pasta lassagnia = new Pasta() {
-        lassagnia.getCooked();
-        lassagnia.getCalories();
-        lassagnia.getIngredients();
-        }
-        Burger bigMac = new Burger() {
-
-        bigMac.getCooked();
-        bigMac.getCalories();
-        bigMac.getIngredients();
-        }
-            Steak beefSteak = new Steak() {
-                @Override
-                public void getCooked()
-                beefSteak.getCooked();
-                 beefSteak.getCalories();
-                 beefSteak.getIngredients();
-            }
-        }
-        BBQ porkRibs = new BBQ() {
-
-        porkRibs.getCooked();
-        porkRibs.getCalories();
-        porkRibs.getIngredients();
+//        Pasta lassagnia = new Pasta();
+//        lassagnia.getCooked();
+//        lassagnia.getCalories();
+//        lassagnia.getIngredients();
+//
+//        Burger bigMac = new Burger();
+//        bigMac.getCooked();
+//        bigMac.getCalories();
+//        bigMac.getIngredients();
+//        Steak beefSteak = new Steak();
+//
+//
+//        beefSteak.getCooked();
+//        beefSteak.getCalories();
+//        beefSteak.getIngredients();
+//        BBQ porkRibs = new BBQ();
+//
+//        porkRibs.getCooked();
+//        porkRibs.getCalories();
+//        porkRibs.getIngredients();
     }
 }
 
