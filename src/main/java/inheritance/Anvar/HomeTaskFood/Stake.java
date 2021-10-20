a4b1e93ddc17f84a8c66f6609d35002257c0e32f
anvark1976 <92128415+anvark1976@users.noreply.github.com>
2021-10-20 19:11:52 -0400

    last

---
+++ src/main/java/inheritance/Anvar/HomeTaskFood/Stake.java
@@ -0,0 +1,16 @@
+package inheritance.Anvar.HomeTaskFood;
+
+public class Stake extends Food{
+
+
+    public void cook() {
+        System.out.println("Restaurants cooking classic stakes");
+    }
+    public void ingredients (){
+        System.out.println("Cooks with meat and peppers . There are 679 calories");
+    }
+    public void Eat (){
+        System.out.println("We like to eat stake");
+    }
+}
+
--- src/main/java/inheritance/Anvar/TaskDocktor/FamilyDoctor.java
+++ src/main/java/inheritance/Anvar/TaskDocktor/FamilyDoctor.java
@@ -2,7 +2,6 @@ package inheritance.Anvar.TaskDocktor;
 
 public class FamilyDoctor extends Doctors {
 
-    public void treat(){
-        System.out.println("Family doctor should give advice");
-    }
+    public void treat(){System.out.println("Family doctor should give advice");}
+
 }
