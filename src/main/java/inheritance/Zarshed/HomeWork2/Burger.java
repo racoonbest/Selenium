60f21ff042811c985259a8cfbb6596f1a96d1f4d
zarshed1997 <92128200+zarshed1997@users.noreply.github.com>
2021-10-20 16:39:33 -0400

    HW

---
+++ src/main/java/inheritance/Zarshed/HomeWork2/Burger.java
@@ -0,0 +1,10 @@
+package inheritance.Zarshed.HomeWork2;
+
+public class Burger extends Food{
+    public void ingredients(){
+        System.out.println("meat,tomato, lettuce, ketchup,onion,bread");
+    }
+    public void calorie(){
+        System.out.println("354 calories");
+    }
+}
---
+++ src/main/java/inheritance/Zarshed/HomeWork2/Main.java
@@ -0,0 +1,30 @@
+package inheritance.Zarshed.HomeWork2;
+
+public class Main {
+    public static void main(String[] args) {
+      BBQ barbeque=new BBQ();
+      barbeque.eaten();
+      barbeque.getCooked();
+      barbeque.ingredients();
+      barbeque.calorie();
+
+      Burger newYork=new Burger();
+      newYork.eaten();
+      newYork.getCooked();
+      newYork.ingredients();
+      newYork.calorie();
+
+      Pasta spaghetti=new Pasta();
+      spaghetti.eaten();
+      spaghetti.getCooked();
+      spaghetti.ingredients();
+      spaghetti.calorie();
+
+      Steak surprise =new Steak();
+      surprise.eaten();
+      surprise.getCooked();
+      surprise.ingredients();
+      surprise.calorie();
+
+    }
+}
