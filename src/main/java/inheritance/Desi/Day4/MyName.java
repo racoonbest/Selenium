eff33cf8dc380cc23019b0df3bdff6e09025fba7
desislavalyubomirova <92128196+desislavalyubomirova@users.noreply.github.com>
2021-10-20 20:46:18 -0400

    movies

---
+++ src/main/java/inheritance/Desi/Day4/MyName.java
@@ -0,0 +1,19 @@
+package inheritance.Desi.Day4;
+
+public class MyName extends Movie {
+    public void play(){
+        System.out.println("Don't play this movie");
+    }
+    public void stop(){
+        System.out.println("Stop the movie");
+    }
+    public void like(){
+        System.out.println("Do you like this movie?");
+    }
+    public void unlike(){
+        System.out.println("May be you don't like the movie");
+    }
+    public void description(){
+        System.out.println("Is about unique name");
+    }
+}
--- src/main/java/inheritance/Desi/FoodHomework/Main7.java
+++ src/main/java/inheritance/Desi/FoodHomework/Main7.java
@@ -2,26 +2,25 @@ package inheritance.Desi.FoodHomework;
 
 public class Main7 {
     public static void main(String[] args) {
-//        Pasta lassagnia = new Pasta();
-//        lassagnia.getCooked();
-//        lassagnia.getCalories();
-//        lassagnia.getIngredients();
-//
-//        Burger bigMac = new Burger();
-//        bigMac.getCooked();
-//        bigMac.getCalories();
-//        bigMac.getIngredients();
-//        Steak beefSteak = new Steak();
-//
-//
-//        beefSteak.getCooked();
-//        beefSteak.getCalories();
-//        beefSteak.getIngredients();
-//        BBQ porkRibs = new BBQ();
-//
-//        porkRibs.getCooked();
-//        porkRibs.getCalories();
-//        porkRibs.getIngredients();
+      Pasta lassagnia = new Pasta();
+       lassagnia.getCooked();
+       lassagnia.getCalories();
+       lassagnia.getIngredients();
+
+       Burger bigMac = new Burger();
+       bigMac.getCooked();
+       bigMac.getCalories();
+      bigMac.getIngredients();
+
+       Steak beefSteak = new Steak();
+       beefSteak.getCooked();
+      beefSteak.getCalories();
+       beefSteak.getIngredients();
+
+       BBQ porkRibs = new BBQ();
+       porkRibs.getCooked();
+       porkRibs.getCalories();
+       porkRibs.getIngredients();
     }
 }
 
