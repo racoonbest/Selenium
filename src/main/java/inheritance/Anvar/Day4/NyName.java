d28fff5ad87b06db2c02fe4c6ed0b9fe4322f867
anvark1976 <92128415+anvark1976@users.noreply.github.com>
2021-10-20 20:41:41 -0400

    Anvar

---
+++ src/main/java/inheritance/Anvar/Day4/NyName.java
@@ -0,0 +1,20 @@
+package inheritance.Anvar.Day4;
+
+public class NyName extends Movies{
+
+    public void play (){
+        System.out.println("play music");
+    }
+    public void stop (){
+        System.out.println("stop movie");
+    }
+    public void like(){
+        System.out.println("like MyName");
+    }
+    public void unlike (){
+        System.out.println("unlike movie");
+    }
+    public void description(){
+        System.out.println("description");
+    }
+}
