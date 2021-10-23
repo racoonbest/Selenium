8703a8570e32cf96c0b265bdf2632a8555a5cada
zarshed1997 <92128200+zarshed1997@users.noreply.github.com>
2021-10-23 10:00:25 -0400

    HW

---
+++ src/main/java/inheritance/Zarshed/HomeWork5/KitchenTool.java
@@ -0,0 +1,8 @@
+package inheritance.Zarshed.HomeWork5;
+
+public abstract class KitchenTool {
+    public void clean(){
+        System.out.println("nice and clean");
+    }
+    public abstract void knifeMaterial();
+}
