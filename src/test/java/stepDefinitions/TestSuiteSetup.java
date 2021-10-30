6f19f57ac70d85453bf5603b0a873c6a0b3fa1b6
racoonbest <91798907+racoonbest@users.noreply.github.com>
2021-10-29 20:16:21 -0400

    Revert "adding writer class"

--- src/test/java/stepDefinitions/TestSuiteSetup.java
+++ src/test/java/stepDefinitions/TestSuiteSetup.java
@@ -7,32 +7,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class TestSuiteSetup{
 
     public static WebDriver chrome;
     public static WebDriverWait wait;
-    public static BufferedWriter writer;
 
     @Before()
-    public void setupSuite() throws IOException {
+    public void setupSuite() {
         WebDriverManager.chromedriver().setup();
         chrome = new ChromeDriver();
         chrome.manage().deleteAllCookies();
         wait = new WebDriverWait(chrome, 20);
-
     }
 
     @After()
-    public void tearDown() throws IOException {
+    public void tearDown() {
         chrome.quit();
-        if( writer != null){
-            writer.close();
-        }
-
     }
 
 }
